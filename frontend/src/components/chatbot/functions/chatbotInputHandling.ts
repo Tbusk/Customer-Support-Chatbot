84635bfb9b6a48dfbcf6f4c2595ad92f0566ee89
Trevor Busk <buskt@ferris.edu>
2025-05-02 01:13:19 -0400

    making sender of messages capitalized

--- frontend/src/components/chatbot/functions/chatbotInputHandling.ts
+++ frontend/src/components/chatbot/functions/chatbotInputHandling.ts
@@ -15,19 +15,19 @@ export default async function handleChatbotInput(props: ChatbotInputFunctionProp
 
         if (input.value.trim()) {
 
-            const messagesWithNewUserMessage:Message[] = [...props.messages, createNewMessage(input.value.trim(), 'user')];
+            const messagesWithNewUserMessage:Message[] = [...props.messages, createNewMessage(input.value.trim(), 'User')];
             props.setMessages(messagesWithNewUserMessage)
 
             const userMessage = input.value.trim();
             input.value = '';
 
-            const messagesWithLoadingMessage: Message[] = [...messagesWithNewUserMessage, createNewMessage('...', 'support')];
+            const messagesWithLoadingMessage: Message[] = [...messagesWithNewUserMessage, createNewMessage('...', 'Support')];
             props.setMessages(messagesWithLoadingMessage);
 
             const data: string = await getResponse(userMessage);
 
             const updatedMessages = messagesWithLoadingMessage.filter(msg => msg.text !== '...');
-            props.setMessages([...updatedMessages, createNewMessage(data, 'support')]);
+            props.setMessages([...updatedMessages, createNewMessage(data, 'Support')]);
         }
     }
 }
