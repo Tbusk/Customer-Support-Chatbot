2b445183f8c861054c25cb487b156158cc71633d
Trevor Busk <buskt@ferris.edu>
2025-05-01 20:24:44 -0400

    new view `DefaultSupportMessage.tsx` for support messages
    - This configures what the first message is (default) when the chatbot is opened for the first time that appears to be sent by the assistant.
    - the default message is built and put into the Support Message view.

---
+++ frontend/src/components/chatbot/messages/DefaultSupportMessage.tsx
@@ -0,0 +1,20 @@
+import SupportMessage from "./SupportMessage.tsx";
+import * as React from "react";
+import {Message} from "../ChatBot.tsx";
+
+export interface DefaultSupportMessageProps {
+    propKey: React.Key;
+}
+
+export default function DefaultSupportMessage(props: DefaultSupportMessageProps) {
+
+    const supportMessage:string = 'How can we assist you today?';
+    const sender: string = 'support';
+    const timestamp: Date = new Date();
+
+    const message:Message = {text: supportMessage, sender: sender, timestamp: timestamp};
+
+    return (
+        <SupportMessage propKey={props.propKey} message={message} />
+    );
+}
