301af58a7c4829705244ee4da2ca5d1879502980
Trevor Busk <buskt@ferris.edu>
2025-05-01 20:21:23 -0400

    new view `SupportMessage.tsx` for support messages
    - In the chatbot window, this configures how the assistant's messages are displayed.
    - It is displayed as a dark grey box with slightly rounded corners and a small but light border with heavy light border to the left side.  It displays the message's creation timestamp in the form of time in the left corner below it.
    - The messages will be aligned to the left side in as small of a box that is needed.
    - The messages are displayed in markdown format.

---
+++ frontend/src/components/chatbot/messages/SupportMessage.tsx
@@ -0,0 +1,22 @@
+import {Text} from "@radix-ui/themes";
+import {Message} from "../ChatBot.tsx";
+import ReactMarkdown from "react-markdown";
+import * as React from "react";
+
+export interface ChatbotSupportMessageProps {
+    message: Message;
+    propKey: React.Key;
+}
+
+export default function SupportMessage(props: ChatbotSupportMessageProps) {
+    return (
+        <div key={props.propKey} className="content-start flex flex-col items-start px-3">
+            <div className="bg-stone-800 p-3 rounded-lg mb-2 text-sm text-gray-200 border border-slate-500 border-l-slate-400 border-l-8">
+                <ReactMarkdown>{props.message.text}</ReactMarkdown>
+            </div>
+            <Text className="text-gray-400  mt-0 mb-2" style={{fontSize:'11px'}}>
+                {props.message.timestamp.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}
+            </Text>
+        </div>
+    );
+}
